8f7078fc5c41a5846acf04d054a8938f220a14e7
ramabna <66891940+ramabna@users.noreply.github.com>
2020-07-13 17:20:49 +0700

    Update function1.php

--- function1.php
+++ function1.php
@@ -9,7 +9,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -5.1722".mt_rand(100,999).",119.418".mt_rand(1000,9999);
+$header[] = "X-Location: -5.1385".mt_rand(100,999).",119.418".mt_rand(1000,9999);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
