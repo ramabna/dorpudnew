29177eb94d0abbd36327a7741e5cef7a58ba1db5
ramabna <66891940+ramabna@users.noreply.github.com>
2020-08-16 18:43:26 +0700

    Update dor15.php

--- dor15.php
+++ dor15.php
@@ -27,7 +27,7 @@ echo "\n".color("white","Claim?: y/n ");
         echo color("white",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0607"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green"," Message: ".$message);
